158ca3147f325246ce6d403a19495b55631e2259
Paul <dev.psimmerling@gmail.com>
2016-02-22 12:57:28 -0500

    Push

--- FRC 496 Stronghold/src/org/usfirst/frc/team496/robot/Robot.java
+++ FRC 496 Stronghold/src/org/usfirst/frc/team496/robot/Robot.java
@@ -3,22 +3,31 @@ package org.usfirst.frc.team496.robot;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.RobotDrive;
+//import edu.wpi.first.wpilibj.AnalogGyro;//necessary?
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.Timer;
+//import edu.wpi.first.wpilibj.Ultrasonic;
+//import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends SampleRobot {
     RobotDrive myRobot;
     Joystick driveStick, opStick;
-    Talon portMotor, starMotor, armPortMotor, armStarMotor;
+    Talon portMotor, starMotor, armPort, armStar;
     
-    //Compresssor Compressor;
+    Compressor compressor;
     DoubleSolenoid portSole, starSole;
     DigitalInput armLimit;
+    
+    //Gyro gyro;//What kind of gyro?
+    
+    //Ultrasonic fUltra, sUltra;
+    
     
     PowerDistributionPanel pdp;
     
@@ -27,18 +36,14 @@ public class Robot extends SampleRobot {
     public static final int armPortMotorPWM = 	2;
     public static final int armStarMotorPWM = 	3;
     
-    public static final int portSoleChanIn = 	0;
-    public static final int portSoleChanOut = 	1;
-    public static final int starSoleChanIn = 	2;
-    public static final int starSoleChanOut = 	3;
+    public static final int portSoleChanIn = 	4;
+    public static final int portSoleChanOut = 	5;
+    public static final int starSoleChanIn = 	6;
+    public static final int starSoleChanOut = 	7;
     
-    public static final int armLimitChan = 4;
+    //public static final int armLimitChan = 4;
     boolean val = false;
     
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    SendableChooser chooser;
-
     public Robot() {
         myRobot = new RobotDrive(0, 1);
         myRobot.setExpiration(0.1);
@@ -47,12 +52,16 @@ public class Robot extends SampleRobot {
         
         portMotor = new Talon(portMotorPWM);
         starMotor = new Talon(starMotorPWM);
-        armPortMotor = new Talon(armPortMotorPWM);
-        armStarMotor = new Talon(armStarMotorPWM);
+        armPort = new Talon(armPortMotorPWM);
+        armStar = new Talon(armStarMotorPWM);
+        
+        compressor = new Compressor();
         
         portSole = new DoubleSolenoid(portSoleChanIn, portSoleChanOut);
         starSole = new DoubleSolenoid(starSoleChanIn, starSoleChanOut);
-        armLimit = new DigitalInput(armLimitChan);
+        //armLimit = new DigitalInput(armLimitChan);
+        
+        //gyro = new AnalogGyro(1);
         
         
         pdp = new PowerDistributionPanel();
@@ -60,33 +69,11 @@ public class Robot extends SampleRobot {
     }
     
     public void robotInit() {
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
-        SmartDashboard.putData("Auto modes", chooser);
+      
     }
 
     public void autonomous() {
     	
-    	String autoSelected = (String) chooser.getSelected();
-//		String autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
-    	
-    	switch(autoSelected) {
-    	case customAuto:
-            myRobot.setSafetyEnabled(false);
-            myRobot.drive(-0.5, 1.0);	// spin at half speed
-            Timer.delay(2.0);		//    for 2 seconds
-            myRobot.drive(0.0, 0.0);	// stop robot
-            break;
-    	case defaultAuto:
-    	default:
-            myRobot.setSafetyEnabled(false);
-            myRobot.drive(-0.5, 0.0);	// drive forwards half speed
-            Timer.delay(2.0);		//    for 2 seconds
-            myRobot.drive(0.0, 0.0);	// stop robot
-            break;
-    	}
     }
 
     public void operatorControl() {
@@ -99,28 +86,27 @@ public class Robot extends SampleRobot {
 			SmartDashboard.putBoolean("Arm Up?", armLimit.get());
 			SmartDashboard.putBoolean("Arm extended?", val);
 			
-        	myRobot.arcadeDrive(driveStick); // drive with arcade style (use right driveStick)
+        	myRobot.arcadeDrive(driveStick.getY(), driveStick.getTwist()); // drive with arcade style (use right driveStick)
             
-        	armPortMotor.set(opStick.getY());
-        	armPortMotor.set(opStick.getY());
+        	armPort.set(-opStick.getY());
+        	armPort.set(opStick.getY());
         	
         	if(opStick.getTrigger())
         	{
         		if(!val)
         		{
-            		portSole.set(DoubleSolenoid.Value.kForward);
+            		portSole.set(DoubleSolenoid.Value.kReverse);
             		starSole.set(DoubleSolenoid.Value.kForward);
             		val = !val;
         		}
         		else
         		{
-            		portSole.set(DoubleSolenoid.Value.kReverse);
+            		portSole.set(DoubleSolenoid.Value.kForward);
             		starSole.set(DoubleSolenoid.Value.kReverse);
             		val = !val;
         		}
 
         	}
-        	
             Timer.delay(0.005);		// wait for a motor update time
         }
     }
@@ -128,9 +114,3 @@ public class Robot extends SampleRobot {
     public void test() {
     }
 }
-
-
-
-
-
-
